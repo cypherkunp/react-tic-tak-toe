d8967a2d654a0ccd403ff35dd90359e077b80527
devvratshukla <devvratshukla@gmail.com>
2020-05-13 07:07:57 +0000

    Deploying to gh-pages from  @ fc0a7cdc70db46e5ac0624c2b102cfc72716ef2c 🚀

---
+++ precache-manifest.2f6c94a32576533d310c001f04d23dcc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba8a366cb32958834d4651bdce96cd6e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "059860c9e717534994cb",
+    "url": "/static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "db72afc33590bd16db60",
+    "url": "/static/css/main.0a8733a7.chunk.css"
+  },
+  {
+    "revision": "059860c9e717534994cb",
+    "url": "/static/js/2.2706ac8b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2706ac8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db72afc33590bd16db60",
+    "url": "/static/js/main.cd146d64.chunk.js"
+  },
+  {
+    "revision": "c294d461ea6b07fef7ff",
+    "url": "/static/js/runtime-main.58ca528c.js"
+  }
+]);
